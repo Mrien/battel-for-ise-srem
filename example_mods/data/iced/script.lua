d3940c22dbac1c74528397a816920fc6c7e956fc
Sponcer <82266969+Mrien@users.noreply.github.com>
2022-07-07 20:23:52 +0800

    awesome

--- example_mods/data/iced/script.lua
+++ example_mods/data/iced/script.lua
@@ -1,20 +1,50 @@
 function onCreatePost()
-    makeLuaText("songComposer", "Composed by morry", 500, 30, 70)
-    setTextAlignment("songComposer", "left")
-    addLuaText("songComposer")
-    
-    makeLuaText("message", "Ron Engine (Modified PE "..version..")", 500, 30, 50)
-    setTextAlignment("message", "left")
-    addLuaText("message")
+    if difficulty == 3 then
+        makeLuaText("message3", "YOU WILL PERISH", 500, 30, 110)
+        setTextAlignment("message3", "left")
+        addLuaText("message3")
 
-    makeLuaText("engineText", "Iced - VS Ron: TBFTIC", 500, 30, 30)
-    setTextAlignment("engineText", "left")
-    addLuaText("engineText")
+        makeLuaText("message2", "Cool guy mode", 500, 30, 90)
+        setTextAlignment("message2", "left")
+        addLuaText("message2")
 
-    if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
-        setProperty('songComposer.y', 680)
-        setProperty('message.y', 660)
-        setProperty('engineText.y', 640)
+        makeLuaText("songComposer", "Composed by morry", 500, 30, 70)
+        setTextAlignment("songComposer", "left")
+        addLuaText("songComposer")
+     
+        makeLuaText("message", "Ron Engine (Modified PE "..version..")", 500, 30, 50)
+        setTextAlignment("message", "left")
+        addLuaText("message")
+
+        makeLuaText("engineText", "Iced Cool - VS Ron: TBFTIC", 500, 30, 30)
+        setTextAlignment("engineText", "left")
+        addLuaText("engineText")
+
+        if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+            setProperty('message3.y', 680)
+            setProperty('message2.y', 660)
+            setProperty('songComposer.y', 640)
+            setProperty('message.y', 620)
+            setProperty('engineText.y', 600)
+        end
+    else 
+        makeLuaText("songComposer", "Composed by morry", 500, 30, 70)
+        setTextAlignment("songComposer", "left")
+        addLuaText("songComposer")
+     
+        makeLuaText("message", "Ron Engine (Modified PE "..version..")", 500, 30, 50)
+        setTextAlignment("message", "left")
+        addLuaText("message")
+
+        makeLuaText("engineText", "Iced - VS Ron: TBFTIC", 500, 30, 30)
+        setTextAlignment("engineText", "left")
+        addLuaText("engineText")
+
+        if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+            setProperty('songComposer.y', 680)
+            setProperty('message.y', 660)
+            setProperty('engineText.y', 640)
+        end
     end
 end
 
