6990e280e42f3e761b903b1841fd4d6bf4e4f65a
Sponcer <82266969+Mrien@users.noreply.github.com>
2022-07-09 09:56:12 +0800

    real

---
+++ example_mods/data/iced/script2.lua
@@ -0,0 +1,6 @@
+function onUpdatePost(elapsed)
+    if difficulty == 2 then
+        setPropertyFromClass("openfl.Lib", "application.window.title", "V.S. Ron: The Battle For Ice Cream - Iced [Hard]")
+    if difficulty == 3 then
+        setPropertyFromClass("openfl.Lib", "application.window.title", "V.S. Ron: The Battle For Ice Cream - Iced [Cool] Ron's POV")
+end
